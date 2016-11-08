67fe62654366c04d73aaf1b064d4088a0d8a9b4a
Justin Love <git@JustinLove.name>
2016-11-07 18:32:12 -0600

    replace locked identity

---
+++ test/server/replace_locked_identity_test.rb
@@ -0,0 +1,52 @@
+require_relative 'test_helper'
+
+class CheckSqrlReplaceLockedIdentity < SqrlTest
+  def before_all
+    current = SQRL::Key::IdentityUnlock.new
+    previous = SQRL::Key::IdentityUnlock.new
+    url = 'http://localhost:3000'
+    session = create_session(url, [previous.identity_master_key])
+    lock = previous.identity_lock_key.unlock_pair
+    @suk = lock[:suk]
+    @preflight = post(session) {|req|
+      req.ident!.setlock(lock)
+    }
+
+    session = create_session(url, [current.identity_master_key, previous.identity_master_key])
+    @query = post(session) {|req| req.query! }
+    ursk = SQRL::Key::UnlockRequestSigning.new(suk, previous)
+    @ident = post(session) {|req| req.ident!.unlock(ursk) }
+  end
+
+  attr_reader :suk
+  attr_reader :preflight
+  attr_reader :query
+  attr_reader :ident
+
+  assert_flags :preflight, {
+    :command_failed         => false,
+  }
+
+  assert_known_version :query
+
+  assert_flags :query, {
+    :id_match               => false,
+    :previous_id_match      => TRUE,
+  }
+
+  def test_server_returns_suk
+    assert_equal(query.suk, suk.b)
+  end
+
+  assert_flags :ident, {
+    :id_match               => TRUE,
+    :previous_id_match      => false,
+    :ip_match               => TRUE,
+    :sqrl_disabled          => false,
+    :function_not_supported => false,
+    :transient_error        => false,
+    :command_failed         => false,
+    :client_failure         => false,
+    :bad_association_id     => false,
+  }
+end
