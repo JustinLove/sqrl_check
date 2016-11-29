9daf41c08b768aab6f49220b0d0d21ce52b2872d
Justin Love <git@JustinLove.name>
2016-11-29 12:15:49 -0600

    check for required fields

--- lib/sqrl/check/server/query_test.rb
+++ lib/sqrl/check/server/query_test.rb
@@ -12,6 +12,29 @@ class SQRL::Check::Server::QueryWithNewIdentity < SQRL::Check::Server::Test
 
   assert_known_version :parsed
 
+  def test_ver_must_be_the_first_parameter
+    assert_equal(@parsed.params.keys.index('ver'), 0)
+  end
+
+  def test_response_must_include_nut_parameter
+    assert_includes(@parsed.params.keys, 'nut')
+  end
+
+  def test_response_must_include_tif_parameter
+    assert_includes(@parsed.params.keys, 'tif')
+  end
+
+  def test_response_must_include_qry_parameter
+    assert_includes(@parsed.params.keys, 'qry')
+  end
+
+  def test_response_qry_should_not_include_scheme_host_or_port
+    uri = URI(@parsed.params['qry'])
+    assert_nil(uri.scheme)
+    assert_nil(uri.host)
+    assert_nil(uri.port)
+  end
+
   assert_flags :parsed, {
     :id_match               => false,
     :previous_id_match      => false,
